52ba097634984e5e1d8492a18829c4df13fc3de9
Sravan Kumar <66896592+sravangorati2001@users.noreply.github.com>
2021-07-01 07:35:56 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,8 +2,12 @@ var map;
 window.addEventListener('load',mapDisplay(28.7041, 77.1025));
 
     $('#submit').click(function(){
+      var ip=$('#IP').val();
+      if(ip==="")
+      alert("please enter IP Address");
+      else{
       map.remove();
-        var ip=$('#IP').val();
+       
         var API_KEY="44b5154c6e7b4a418847327bf7840615";
 
        var url="https://api.ipgeolocation.io/ipgeo?apiKey="+API_KEY+"&ip="+ip;
@@ -11,13 +15,15 @@ window.addEventListener('load',mapDisplay(28.7041, 77.1025));
        $.get(url,function(data){
       var latitude=parseFloat(data.latitude);
       var longitude=parseFloat(data.longitude);
-     
+      console.log(data);
+      
         $('#city').html(data.city)
         $('#country').html(data.country_name);
         $('#flag').attr("src",data.country_flag)
        mapDisplay(latitude,longitude);
       
        });
+      }
     });
 
 
@@ -27,10 +33,12 @@ function mapDisplay(latitude,longitude){
     zoom: 10
  }
  
+ // Creating a map object
  map = new L.map('map', mapOptions);
-
+ 
+ // Creating a Layer object
  var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
   L.marker([latitude,longitude]).addTo(map);
-
+ // Adding layer to the map
  map.addLayer(layer);
 }
